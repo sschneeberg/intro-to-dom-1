c5f8bb854941a660ecd7d0746b34db07c67a71a1
famousPete <pete.macaluso@generalassemb.ly>
2020-10-22 11:05:16 -0400

    instructions in app.js!

---
+++ app.js
@@ -0,0 +1,46 @@
+// Touring the DOM zoo!
+// Select the specified parts of the DOM and console.log them
+// use document.getElementById, document.getElementsByClassName, document.getElementsByTagName
+
+// the mammals section
+// the birds section
+// all divs with the class animal
+// the first animal div
+// the last animal div
+// all imgs
+// the number of imgs on the page
+// all labels
+// all the labels, one at a time
+
+// Do the zoo tour one more time, this time with document.querySelector and document.querySelectorAll
+
+
+// Changing parts of the zoo!
+
+// The owl is the coolest animal. Give the owl img a gold border to show this
+// Change the label for the bunny to "Stylish Bunny"
+// Once with innerText, then again with innerHTML. When using innerHTML, create a new div inside the label tag
+// Give all images a border-radius of 3em
+
+// The parrot is being rotated for a newer, flashier model: https://images.pexels.com/photos/40984/animal-ara-macao-beak-bird-40984.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500. Change the img's src to reflect this. Use a direct attribute assignment, not the setAttribute method
+
+// Change our sheep image to: https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500. Use setAttribute
+
+// Give all labels a text-decoration of underline, a font-size of 2em, and a color of darkgreen. Create a new class to handle this, and apply it to all the labels.
+
+// Remove the animal class from all divs that have it
+
+
+
+// Zoo events!
+
+// Put all of our existing js inside of a DOMContentLoaded event
+
+// attach an event listener to the owl image that logs "You clicked the coolest animal!"
+
+// attach an event listener to each animal that logs "You clicked an animal!"
+
+// Change the event listener on each animal to log "You clicked on <the-animal's-name>!". First do this the long way w/ 6 different events, then use a loop and the event object
+
+// Add an event listener to each animal picture that changes that picture's label to 'Hello!' when the image is clicked on
+
